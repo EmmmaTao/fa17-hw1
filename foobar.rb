b462d9ff87a769888f659386bfc1d169b5b1510a
Emma Tao <jiayue_tao@berkeley.edu>
2017-09-19 17:54:21 -0700

    completed

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,10 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.map {|e| e.to_i + 2 if (e.to_i + 2) % 2 == 0 and (e.to_i + 2) < 10}.uniq
+    sum = 0
+    a.each {|x| sum += x if x.is_a?Integer}
+    return sum
   end
 end
 
